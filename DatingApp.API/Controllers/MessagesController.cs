efcdec33a86815fb3852ec9e27c1f0820bd642bf
kiruthiharan <kiruthiharan@gmail.com>
2020-03-30 18:27:31 +0530

    message in api done

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -26,7 +27,7 @@ namespace DatingApp.API.Controllers
             _repo = repo;
         }
 
-        [HttpGet("{id", Name="GetMessage")]
+        [HttpGet("{id}", Name="GetMessage")]
         public async Task<IActionResult> GetMessage(int userId, int id) {
             
             if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
@@ -39,6 +40,38 @@ namespace DatingApp.API.Controllers
                 return NotFound();
 
             return Ok(messageFromRepo);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessages(int userId, [FromQuery]MessageParams messageParams) {
+            
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+                return Unauthorized();
+            }
+
+            messageParams.UserId = userId;
+
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize, messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+
+            return Ok(messages);
+        }
+
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId) {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+                return Unauthorized();
+            }
+
+            var messagesFromRepo = await _repo.GetMessageThread(userId,recipientId);
+
+            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            return Ok(messageThread);
         }
 
         [HttpPost]
@@ -60,7 +93,8 @@ namespace DatingApp.API.Controllers
             _repo.Add(message);
 
             if(await _repo.SaveAll()){
-                CreatedAtRoute("GetMessage", new {id = message.Id}, message);
+                //return CreatedAtRoute(nameof(GetMessage), new {id = message.Id}, message);
+                return Ok();
             }
 
             throw new Exception("Creating the message failed on save");
